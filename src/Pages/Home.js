d012fce8484a0eb1c9542b0651f6c34f75804bdf
fuadriyadhi <fuadriyadhi678@gmail.com>
2022-09-23 19:07:03 +0700

    update

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -11,12 +11,13 @@ export default function Home() {
     app.innerHTML += Navbar();
     res.data.forEach((e) => {
       app.innerHTML += `
-      <a href="/details?id=${e.id}" class="flex flex-col gap-4 max-w-[900px] shadow-lg mx-auto my-6 p-6">
+      <div class="flex flex-col gap-4 max-w-[900px] shadow-lg mx-auto my-6 p-6 bg-white rounded-md">
         <h1 class="text-4xl">${e.judul}</h1>
         <small>${e.author} - ${e.createdAt}</small>
         <img src="${e.img} alt="${e.judul}"  class="w-full h-[280px] my-4 object-cover rounded-lg"/>
         <p>${e.content}</p>
-      </a>
+        <a class="bg-slate-700 w-[22%] text-center py-2 rounded-md ml-auto text-white" href="/details?id=${e.id}">Lihat Selengkapnya >>></a>
+      </div>
       `;
     });
   });
